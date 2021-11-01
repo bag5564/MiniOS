677ff716833778758e7699930eca2dffb61bedc3
Benjamin Gutierrez <bag5564@psu.edu>
2021-10-31 22:03:18 -0400

    implemented page table Q3

--- kernel/kernel_code.c
+++ kernel/kernel_code.c
@@ -15,6 +15,30 @@ void *page_table = NULL; /* TODO: Must be initialized to the page table address 
 void *user_stack = NULL; /* TODO: Must be initialized to a user stack virtual address */
 void *user_program = NULL; /* TODO: Must be initialized to a user program virtual address */
 
+typedef unsigned long long u64;
+
+// structure used to populate Level 1 entries
+struct page_pte {
+	u64 present:1; 		// bit P
+	u64 writable:1;		// bit R/W
+	u64 user_mode:1;	// bit U/S
+	u64 otherbits:9;
+	u64 page_address:40;	// physical address
+	u64 avail:7;		// reserved
+	u64 pke:4;		// MPK/MPE
+	u64 nonexecute:1;
+};
+// structure used to populate Level 2, 3, or 4 entries
+struct page_pde {
+	u64 present:1;		// bit P
+	u64 writable:1;		// bit R/W
+	u64 user_mode:1;	// bit U/S
+	u64 otherbits:9;
+	u64 page_address:40;	// physical address
+	u64 avail:11;		// reserved
+	u64 nonexecute:1;
+};
+
 void kernel_init(void *ustack, void *uprogram, void *memory, size_t memorySize)
 {
 	// 'memory' points to the place where memory can be used to create
@@ -22,6 +46,61 @@ void kernel_init(void *ustack, void *uprogram, void *memory, size_t memorySize)
 	// 'memorySize' is the maximum allowed size, do not exceed that (given just in case)
 
 	// TODO: Create a page table here
+	// LEVEL 1 tables (2048 tables = 1048576 entries) - PTE 
+	struct page_pte *p = (struct page_pte *) memory;
+	printf("p %p\n",p);
+	for (int i = 0; i < 1048576; i++) {
+		p[i].present = 1;
+		p[i].writable = 1;
+		p[i].user_mode = 0;
+		p[i].otherbits = 0;
+		p[i].page_address = i;
+		p[i].avail = 0;
+		p[i].pke = 0;
+		p[i].nonexecute = 0;
+	}
+	// LEVEL 2 TABLES (4 tables = 2048 entries) - PDE
+	struct page_pde *pd = (struct page_pde *) (p + 1048576);
+	printf("pde %p\n",pd);
+	for (int j = 0; j <2048; j++) {
+		struct page_pte *start_pte = p + 512 * j;
+		pd[j].present = 1;
+		pd[j].writable = 1;
+		pd[j].user_mode = 0;
+		pd[j].otherbits = 0;
+		pd[j].page_address = (u64) start_pte >> 12; // PTE page address
+		pd[j].avail = 0;
+		pd[j].nonexecute = 0;
+	}
+	// LEVEL 3 TABLE (1 table = 512 entries) - PDPE
+	struct page_pde *pdpe = (struct page_pde *) (pd + 2048);
+	printf("pdpe %p\n",pdpe);
+	// initialize 4 entries in table
+	for (int k = 0; k < 4; k++) {
+		struct page_pde *start_pde = pd + 512 * k;
+		pdpe[k].present = 1;
+		pdpe[k].writable = 1;
+		pdpe[k].user_mode = 0;
+		pdpe[k].otherbits = 0;
+		pdpe[k].page_address = (u64) start_pde >> 12; // PDE page address
+		pdpe[k].avail = 0;
+		pdpe[k].nonexecute = 0;
+	}
+	// LEVEL 4 TABLE (1 table = 512 entries) - PMLE4E
+        struct page_pde *pmle4e = (struct page_pde *) (pdpe + 512);
+	printf("pmle4e %p\n", pmle4e);
+	// initialize 1 entry in table
+	struct page_pde *start_pdpe = pdpe;
+	pmle4e[0].present = 1;
+	pmle4e[0].writable = 1;
+	pmle4e[0].user_mode = 0;
+	pmle4e[0].otherbits = 0;
+	pmle4e[0].page_address = (u64) start_pdpe >> 12; // PDPE page address
+	pmle4e[0].avail = 0;
+	pmle4e[0].nonexecute = 0;
+	
+	// initialize page table address
+	page_table = pmle4e;
 
 	// TODO: It is OK for Q1-Q3, but should changed
 	// for the final part's Q4 when creating your own user page table
